3c0998e251320cac0eeb2e973e372b30f4485d13
enripost <enrico.postolov@gmail.com>
2023-08-10 18:05:59 +0200

    Project setup

---
+++ UnitConverter/UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Enrico Postolov on 10/08/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
